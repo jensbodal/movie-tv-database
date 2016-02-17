92009bded510b199b2a49decd6249e8524d4777f
Jens Bodal <jensbodal@gmail.com>
2016-02-16 19:40:10 -0800

    Have good loop working for data

--- index.php
+++ index.php
@@ -20,12 +20,30 @@ if ($conn->connect_error) {
 $sql = "SELECT id, f_name, l_name FROM actor_test";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["f_name"] . "" . $row["l_name"] . "<br>";
-    }
+if ($result->num_rows > 0) { 
+    while ($rows[] = mysqli_fetch_assoc($result));
+    // last entry is empty so remove it
+    array_pop($rows);
+?>
 
+<table>
+    <thead>
+        <td>ID</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Full Name</td>
+    </thead>
+
+<?php foreach($rows as $row): ?>
+    <tr>
+        <td><?=$row['id']?></td>
+        <td><?=$row['f_name']?></td>
+        <td><?=$row['l_name']?></td>
+        <td><?=$row['f_name'] . " " . $row['l_name']?></td>
+    </tr>
+<?php endforeach; ?>
+</table>
+<?
 }
 else {
     echo "0 results";
