6da81e3bbc102732f6da1add5572d08567b6d823
Jens Bodal <bodalj@onid.oregonstate.edu>
2016-03-10 04:24:55 -0800

    Have movie view ready to send request

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,63 @@
+
 <?php
 if (empty($title)) {
     $title = "Actor/Movie Database";
 }
+
+$root_dir = dirname(dirname(__FILE__));
+$pattern = '/\/nfs.*public_html(.*)/';
+preg_match($pattern, $root_dir, $matches);
+$root_dir = $matches[1];
+$css_dir = $root_dir . '/public/stylesheets/style.css';
+$js_dir = $root_dir . '/public/javascripts/htmlTagHandler.js';
+$home_dir = $root_dir . '/';
 
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <title><?php echo $title ?></title>
-    <link rel="stylesheet" href="../public/stylesheets/style.css" type="text/css">
+    <meta charset="UTF-8" />
+    
+    <!-- *********************************   CSS   ********************************* -->
+    <!-- Our stylesheet -->
+    <link rel="stylesheet" href=<?=$css_dir?> type="text/css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css">
+    <!-- Bootstrap Theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" type="text/css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <!-- ********************************* SCRIPTS ********************************* -->
+    <!-- Our layout handler -->
+    <script src=<?=$js_dir?> type="text/javascript"></script>
+    <!-- Jquery 1.12.0 -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href=<?=$home_dir?>>Movie/Actor DB</a>
+        </div>
+        <div id="navbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href=<?=$home_dir?>>Home</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Views <span class="caret"></span></a> 
+                <ul class="dropdown-menu" role="menu">
+                <li><a href=<?=$root_dir."/views/movieView"?>>Movies</a></li>
+                <li><a href=<?=$root_dir."/views/tvshowView"?>>TV Shows</a></li>
+                </ul>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<script>
+$(document.body).append('</html>');
+</script>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title = 'Movie/TV Show Database';
+include 'includes/header.php';
+?>
+
+  <body>
+    <h3> Welcome! What would you like to do? </h3>
+    <a href="views">Browse</a>
+    <a href="add.html">Add Data</a>
+
+  </body>
+</html>
