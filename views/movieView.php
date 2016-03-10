a0a3e37ee1162cea3470f29af67b9f2ac48c9014
Jens Bodal <bodalj@onid.oregonstate.edu>
2016-03-09 22:26:30 -0800

    Working on summarizing genres for movies

--- views/movieView.php
+++ views/movieView.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 'On');
 
 $DBO = new MovieActorDB();
 
-$rows = $DBO->query("SELECT title, release_date, release_country, runtime, content_rating FROM movie ORDER BY title");
+$rows = $DBO->query("SELECT title, release_date, release_country, runtime, content_rating, genre.genre_type FROM movie INNER JOIN movie_genre ON movie_genre.movie_id = movie.id INNER JOIN genre ON genre.id = movie_genre.genre_id ORDER BY title");
 
 if (count($rows) > 0):
 ?>
@@ -23,6 +23,7 @@ if (count($rows) > 0):
         <td>Release Date</td>
         <td>Runtime</td>
         <td>Content Rating</td>
+        <td>Genre(s)</td>
     </thead>
 
 <?php foreach($rows as $row): ?>
@@ -32,6 +33,7 @@ if (count($rows) > 0):
         <td><?=$row['release_country']?></td>
         <td><?=$row['runtime']?></td>
         <td><?=$row['content_rating']?></td>
+        <td><?=$row['genre_type']?></td>
     </tr>
 <?php endforeach; ?>
 
