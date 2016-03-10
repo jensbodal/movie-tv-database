632dedff32c268de2e544dcbdfe6b670b6dfd0da
Jens Bodal <bodalj@onid.oregonstate.edu>
2016-03-10 05:12:50 -0800

    Removed home page active styling

--- includes/header.php
+++ includes/header.php
@@ -46,7 +46,7 @@ $home_dir = $root_dir . '/';
         </div>
         <div id="navbar">
             <ul class="nav navbar-nav">
-                <li class="active"><a href=<?=$home_dir?>>Home</a></li>
+                <li><a href=<?=$home_dir?>>Home</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Views <span class="caret"></span></a> 
                     <ul class="dropdown-menu" role="menu">
