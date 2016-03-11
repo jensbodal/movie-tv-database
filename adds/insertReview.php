e6a37dfc6f0eeb3d91893a12177a62c76186a872
emilyls <snyderem@oregonstate.edu>
2016-03-10 22:11:42 -0800

    submitting a review actually gets inserted into db

--- adds/insertReview.php
+++ adds/insertReview.php
@@ -1,18 +1,19 @@
 <?php
 
-$title = "New Review";
-include 'includes/header.php';
-include 'classes/MovieActorDBO.php';
+$title = "Insert Review";
+  include '../includes/header.php';
+  include '../classes/MovieActorDBO.php';
 
 // turn on errors
 ini_set('display_errors', 'On');
 
 $DBO = new MovieActorDB();
 
+$site = str_replace("+", " ", $_GET['site_name']);
 
 $result = $DBO->query("INSERT INTO rating (media_id, site_id, rating, rating_url) VALUES
 	(".$_GET['media_id'].", 
-  (SELECT site.id FROM site WHERE (site.name = '".$_GET['site_name']."')),
+  (SELECT site.id FROM site WHERE (site.name = '".$site."')),
   ".$_GET['rating'].", '".$_GET['url']."')");
 
 
