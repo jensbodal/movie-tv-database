2e64686282d58a3091130dff3c21979421f8b836
emilyls <snyderem@oregonstate.edu>
2016-03-11 19:18:16 -0800

    did something?

--- adds/getSiteMax.php
+++ adds/getSiteMax.php
@@ -1,17 +1,17 @@
 <?php
 
-$title = "Get Site Max";
-include '../classes/MovieActorDBO.php';
+  $title = "Get Site Max";
+  include '../classes/MovieActorDBO.php';
 
-// turn on errors
-ini_set('display_errors', 'On');
+  // turn on errors
+  ini_set('display_errors', 'On');
 
-$DBO = new MovieActorDB();
+  $DBO = new MovieActorDB();
 
-$site = str_replace("+", " ", $_GET['site_name']);
+  $site = str_replace("+", " ", $_GET['site_name']);
 
-$results = $DBO->queryJSON("SELECT max_rating FROM site WHERE site.name = '".$site."'");
+  $results = $DBO->queryJSON("SELECT max_rating FROM site WHERE site.name = '".$site."'");
 
-echo $results;
+  echo $results;
 
 ?>
